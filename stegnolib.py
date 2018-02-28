6043d1cee41494783071cca062f12b6cdf071273
Md Azmal <azmalmd12@gmail.com>
2018-02-28 18:02:27 +0530

    documented the bit generator

--- stegnolib.py
+++ stegnolib.py
@@ -3,18 +3,19 @@ import numpy as np
 import random 
 
 def bitGen(reader):
+	""" Return the character bit by bit"""
 	for r in reader:
 		asc = ord(r)
 		i = 0
-		while i < 7:
-			yield asc & 1
-			asc = asc >> 1
-			i+=1
-	for i in range(7):
+		while i < 7: # 7 bits in each character
+			yield asc & 1 # returns the right most bit of the character by doing bitwise and with 1
+			asc = asc >> 1 # once the bit has been returned it is shifted to right and removed
+			i+=1           # increment the counter after each yield
+	for i in range(7):     # using null as delemeter to mark the end of the file
 		yield 0
 
 	while True:
-		yield random.randint(0,1)
+		yield random.randint(0,1) # return random 0s and 1s to populate the image
 
 
 def az_lsb_embed(filename,imagename):
