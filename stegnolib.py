4e7429966616a469c16c025841d1b4018a65b7cc
Md Azmal <azmalmd12@gmail.com>
2018-02-28 19:57:00 +0530

    documenting az_lsb_embed function

--- stegnolib.py
+++ stegnolib.py
@@ -30,7 +30,7 @@ def az_lsb_embed(filename,imagename):
 
 	width,height,size = img.shape[1],img.shape[0],img.size
 
-	if len(reader)*7 + 7 > size:
+	if len(reader)*7 + 7 > size: # if length of file with the delimeter is more than what image can hold return
 		print('File size exceeds the range')
 		return
 
@@ -38,21 +38,21 @@ def az_lsb_embed(filename,imagename):
 	end = 0
 	for j in range(height):
 		for i in range(width):
-			if end == len(reader)*7 + 7:
+			if end == len(reader)*7 + 7: # check where the stream would end
 				break
 			end += 3
-			print(i,j)
-			pix = img[j,i].copy()
-			for k in range(3):
-				bit = next(bits)
+			pix = img[j,i].copy() 
+			for k in range(3):  # shorthand to modify BGR in one go
+				bit = next(bits) # iterate over each bit from the file 
 				if bit == 1:
-					pix[k] = pix[k] | bit
+					pix[k] = pix[k] | bit # make the right most bit by using bitwise or with 
 				else:
-					pix[k] = pix[k] & 0b1111110 
+					pix[k] = pix[k] & 0b1111110  # make the right most bit zero by using bitwise and with 1111110
 			img[j,i] = pix
-
 	
 	cv2.imwrite('eimage.png',img)
+
+
 
 
 if __name__ == '__main__':
