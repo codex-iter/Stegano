9af9a6929954029fc3ef08ef878dbe5f95f636ad
Md Azmal <azmalmd12@gmail.com>
2018-03-08 23:11:00 +0530

    testing the new methods

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from steganolib import stegano_az as stg_az
 
-stg_az.lsb_embed('text.txt','image.jpg',1)
-stg_az.lsb_retv('output.txt','eimage.png',1)
+# stg_az.lsb_embed('text.txt','image.jpg',1)
+# stg_az.lsb_retv('output.txt','eimage.png',1)
+
+stg_az.lsb_alpha_embed('text.txt','image.jpg',1)
+stg_az.lsb_alpha_retv('outalpha.txt','eimagealpha.png',1)
