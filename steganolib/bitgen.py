34fcb06fa1e37cf4e6eb3c98d621810a7097cbb4
Md Azmal <azmalmd12@gmail.com>
2018-03-08 23:11:20 +0530

    added support for 8 bit integers too

--- steganolib/bitgen.py
+++ steganolib/bitgen.py
@@ -11,9 +11,15 @@ def bitGen_text(filename):
 	for r in reader:
 		asc = ord(r)
 		i = 0
-		while i < 7: # 7 bits in each character
-			yield asc & 1 # returns the right most bit of the character by doing bitwise and with 1
+		while i < 7:       # 7 bits in each character
+			yield asc & 1  # returns the right most bit of the character by doing bitwise and with 1
 			asc = asc >> 1 # once the bit has been returned it is shifted to right and removed
 			i+=1           # increment the counter after each yield
 	for i in range(7):     # using null as delemeter to mark the end of the file
 		yield 0
+
+def setBit(pix_val,bit):
+	if bit == 1:
+		return pix_val | bit        # make the right most bit by using bitwise or with 
+	else:
+		return pix_val & 0b11111110 # make the right most bit zero by using bitwise and with 11111110 for 8 bit
