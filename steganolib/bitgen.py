aebf261e9544a128933e5e79a3ac161b932878d9
Md Azmal <azmalmd12@gmail.com>
2018-03-18 22:03:08 +0530

    adding chunk generator

--- steganolib/bitgen.py
+++ steganolib/bitgen.py
@@ -1,10 +1,6 @@
 
-def bitGen_text(filename):
+def bitGen_text(reader):
 	""" Return the character bit by bit"""
-
-	file = open(filename,'r')
-	reader = file.read()
-	file.close()
 
 	yield len(reader)   # first yield the file name 
 
@@ -23,3 +19,9 @@ def setBit(pix_val,bit):
 		return pix_val | bit        # make the right most bit by using bitwise or with 
 	else:
 		return pix_val & 0b11111110 # make the right most bit zero by using bitwise and with 11111110 for 8 bit
+
+def chunk(reader,chunk_size):
+	reader_db = []
+	for i in range(0,len(reader),chunk_size):
+		reader_db.append(reader[i:i+chunk_size])
+	return reader_db
