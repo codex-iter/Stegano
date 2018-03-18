a2e93d456e91ca19871c7ffb2d5771de66a2bf5e
Md Azmal <azmalmd12@gmail.com>
2018-03-18 23:13:41 +0530

    issue in out.txt adds garbage

--- split.py
+++ split.py
@@ -1,22 +1,35 @@
 from steganolib import stegano_az as steg_az 
 from steganolib import bitgen as bg
 
-imageloc = ['/home/loopaz/Desktop/image.jpg','/home/loopaz/Desktop/image2.jpg','/home/loopaz/Desktop/image3.jpeg']
+imageloc = ['/home/loopaz/Desktop/image.jpg','/home/loopaz/Desktop/image2.jpg'] #,'/home/loopaz/Desktop/image3.jpeg']
 fileloc = '/home/loopaz/Desktop/text.txt'
 outloc = '/home/loopaz/Desktop/out.txt'
-outimloc = ['/home/loopaz/Desktop/i1.png','/home/loopaz/Desktop/i2.png','/home/loopaz/Desktop/i3.png']
+outimloc = ['/home/loopaz/Desktop/i1.png','/home/loopaz/Desktop/i2.png'] # ,'/home/loopaz/Desktop/i3.png']
 
 file = open(fileloc)
 reader = file.read()
 file.close()
 
-chunk_size = len(reader)//len(imageloc)
+print(len(reader))
+print(len(imageloc))
+
+chunk_size = len(reader) // len(imageloc) + 1
 
 print(chunk_size,'\n\n\n')
 
 db = bg.chunk(reader,chunk_size)
 
-for df in db:
-	print(df)
-	print('\n'*2)
+print(len(db))
+print(db)
 
+
+# for i in range(len(db)):
+# 	steg_az.lsb_embed(db[i],imageloc[i],outimloc[i],1)
+# 	print(db[i],imageloc[i],outimloc[i])
+# 	print('successfully embeded')
+
+# for i in range(len(outimloc)):
+# 	steg_az.lsb_retv(outloc,outimloc[i],1)
+# 	print('retrieved')
+
+steg_az.lsb_retv(outloc,'/home/loopaz/Desktop/i2.png',1)
