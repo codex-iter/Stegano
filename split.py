fc92757d73ead2bf072d5c5b52743884283f5da2
Md Azmal <azmalmd12@gmail.com>
2018-03-19 15:16:25 +0530

    testing split files feature

--- split.py
+++ split.py
@@ -1,7 +1,7 @@
 from steganolib import stegano_az as steg_az 
 from steganolib import bitgen as bg
 
-imageloc = ['/home/loopaz/Desktop/image.jpg','/home/loopaz/Desktop/image2.jpg'] #,'/home/loopaz/Desktop/image3.jpeg']
+imageloc = ['/home/loopaz/Desktop/image.jpg','/home/loopaz/Desktop/image2.jpg'] # ,'/home/loopaz/Desktop/image3.jpeg']
 fileloc = '/home/loopaz/Desktop/text.txt'
 outloc = '/home/loopaz/Desktop/out.txt'
 outimloc = ['/home/loopaz/Desktop/i1.png','/home/loopaz/Desktop/i2.png'] # ,'/home/loopaz/Desktop/i3.png']
@@ -13,23 +13,28 @@ file.close()
 print(len(reader))
 print(len(imageloc))
 
-chunk_size = len(reader) // len(imageloc) + 1
+chunk_size = len(reader) // len(imageloc)
 
 print(chunk_size,'\n\n\n')
 
-db = bg.chunk(reader,chunk_size)
+db = bg.chunk(reader,len(imageloc))
+
+if len(db) > len(imageloc):
+	db[-2] = db[-2] + db[-1]
+	del db[-1]
 
 print(len(db))
 print(db)
 
 
-# for i in range(len(db)):
-# 	steg_az.lsb_embed(db[i],imageloc[i],outimloc[i],1)
-# 	print(db[i],imageloc[i],outimloc[i])
-# 	print('successfully embeded')
+for i in range(len(db)):
+	steg_az.lsb_embed(db[i],imageloc[i],outimloc[i],1)
+	# print(db[i],imageloc[i],outimloc[i])
+	print('successfully embeded')
 
-# for i in range(len(outimloc)):
-# 	steg_az.lsb_retv(outloc,outimloc[i],1)
-# 	print('retrieved')
+for i in range(len(outimloc)):
+	steg_az.lsb_retv(outloc,outimloc[i],1)
+	print('retrieved')
 
-steg_az.lsb_retv(outloc,'/home/loopaz/Desktop/i2.png',1)
+# steg_az.lsb_retv(outloc,'/home/loopaz/Desktop/i1.png',1)
+
