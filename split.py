ee1a6b855bae9c7613bc1e64d054b6fd71a4762f
Md Azmal <azmalmd12@gmail.com>
2018-03-19 15:24:09 +0530

    testing split on 3 images success :)

--- split.py
+++ split.py
@@ -1,17 +1,17 @@
 from steganolib import stegano_az as steg_az 
 from steganolib import bitgen as bg
 
-imageloc = ['/home/loopaz/Desktop/image.jpg','/home/loopaz/Desktop/image2.jpg'] # ,'/home/loopaz/Desktop/image3.jpeg']
+imageloc = ['/home/loopaz/Desktop/image.jpg','/home/loopaz/Desktop/image2.jpg','/home/loopaz/Desktop/image3.jpeg']
 fileloc = '/home/loopaz/Desktop/text.txt'
 outloc = '/home/loopaz/Desktop/out.txt'
-outimloc = ['/home/loopaz/Desktop/i1.png','/home/loopaz/Desktop/i2.png'] # ,'/home/loopaz/Desktop/i3.png']
+outimloc = ['/home/loopaz/Desktop/i1.png','/home/loopaz/Desktop/i2.png','/home/loopaz/Desktop/i3.png']
 
 file = open(fileloc)
 reader = file.read()
 file.close()
 
-print(len(reader))
-print(len(imageloc))
+# print(len(reader))
+# print(len(imageloc))
 
 # chunk_size = len(reader) // len(imageloc)
 
